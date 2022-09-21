2744bec74be0f309618253c6ca81e7496828f90d
varunlambda <108345053+varunlambda@users.noreply.github.com>
2022-09-21 17:26:37 +0530

    Update android-single.conf.js

--- android-sample/android-single.conf.js
+++ android-sample/android-single.conf.js
@@ -1,6 +1,6 @@
 exports.config = {
-  user: process.env.LT_USERNAME || "YOUR_USERNAME",
-  key: process.env.LT_ACCESS_KEY || "YOUR_ACCESS_KEY",
+  user: process.env.LT_USERNAME || "varunkumarb",
+  key: process.env.LT_ACCESS_KEY || "doLbtRzYpWdpD1xxcIQdByQ1b7kmqMNg43DrpsYwGxbPBtphNU",
 
   updateJob: false,
   specs: ["specs/android-test.js"],
